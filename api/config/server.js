a3e67f25e9b7143721b33c53c112faa0f647521d
Jocelim <jocelimjr2000@hotmail.com>
2020-11-26 21:16:54 -0300

    cors

--- api/config/server.js
+++ api/config/server.js
@@ -3,9 +3,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
+
+app.use(cors());
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(routes);
 
